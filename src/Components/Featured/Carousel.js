f31fd4a2b11a89b448fbd84b406d581b14b31d79
Olayiwola Olanrewaju <larry_coal@outlook.com>
2020-08-26 08:13:58 -0600

    carousel

--- src/Components/Featured/Carousel.js
+++ src/Components/Featured/Carousel.js
@@ -7,24 +7,33 @@ import { Flip } from "react-reveal";
 SwiperCore.use([Pagination, Autoplay]);
 
 const Carousel = () => {
-  const slides=[
+  const slides = [
     {
-      img:"https://images.unsplash.com/photo-1465453869711-7e174808ace9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-      text:"Lorem"
+      img:
+        "https://www.lucine-a.com/wp-content/uploads/2018/07/African-Fashion-Festival-3.jpg",
+      text: "Trendy Ankara",
     },
     {
-      img:"https://images.unsplash.com/photo-1560203125-033f79601481?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-      text:"Lorem"
+      img:
+        "https://images.unsplash.com/photo-1591348278900-019a8a2a8b1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      text: "Beautiful Bags",
     },
     {
-      img:"https://images.unsplash.com/photo-1521317955574-882772c69723?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-      text:"Lorem"
+      img:
+      "https://t3.ftcdn.net/jpg/01/77/39/20/240_F_177392031_pCMnvyMiuiwEun0hyRIYaRPxBCJf9PNo.jpg",
+      text: "Stylish Attire",
     },
     {
-      img:"https://images.unsplash.com/photo-1576995853123-5a10305d93c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-      text:"Lorem"
-    }
-  ]
+      img:
+        "https://t4.ftcdn.net/jpg/02/82/49/43/240_F_282494334_om0DBFChHJGhrOeBZyTDNJW9s1r8Koly.jpg",
+      text: "Sneakers",
+    },
+    {
+      img:
+        "https://images.unsplash.com/photo-1587836374828-4dbafa94cf0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      text: "Original Watches",
+    },
+  ];
   const settings = {
     spaceBetween: 0,
     centeredSlides: true,
@@ -36,24 +45,31 @@ const Carousel = () => {
       clickable: true,
     },
   };
-  const showSlides = ()=>{
-    return slides.map((slide)=>(
+  const showSlides = () => {
+    return slides.map((slide) => (
       <SwiperSlide>
         <div
           className="carousel_image"
           style={{
-            background:`url(${slide.img})`
+            background: `url(${slide.img})`,
           }}
         >
-          <h1>
-            <Flip left cascade>
-              {slide.text}
-            </Flip>
-          </h1>
+          <div style={{
+            position:"relative",
+           top:"40%",
+           transform:"translate(-50% -50%)"
+          }}>
+            <h2>Home Of</h2>
+            <h1>
+              <Flip left cascade>
+                {slide.text}
+              </Flip>
+            </h1>
+          </div>
         </div>
       </SwiperSlide>
-    ))
-  }
+    ));
+  };
   return (
     <Swiper {...settings} pagination>
       {showSlides()}
