640e61709af6f02b718bb7abee0d9ecee503f246
Olayiwola Olanrewaju <larry_coal@outlook.com>
2020-09-11 12:07:54 +0100

    scale

--- src/Resources/Data/index.js
+++ src/Resources/Data/index.js
@@ -57,13 +57,55 @@ export const hova=[
         desc:"Black bootcut trouser",
         price:"4000",
         title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598539037/product4_m2ngy8.png",
+        designer:"Hova",
+        desc:"Ankara Corset top.Hand beaded",
+        price:"4000",
+        title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598909853/Screenshot_2020-08-27_at_8.33.20_AM_q1oual.png",
+        designer:"Hova",
+        desc:"The adire 2 piece top set",
+        price:"4000",
+        title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598909823/Screenshot_2020-08-31_at_3.32.53_PM_ge56vd.png",
+        designer:"Hova",
+        desc:"2 piece ankara short and kimono",
+        price:"4000",
+        title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598909785/Screenshot_2020-08-31_at_3.32.34_PM_z4qup9.png",
+        designer:"Hova",
+        desc:"2 piece lounge wear",
+        price:"4000",
+        title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598909740/Screenshot_2020-08-31_at_3.30.16_PM_xfcalp.png",
+        designer:"Hova",
+        desc:"Ankara two way gown",
+        price:"5000",
+        title:"HOVA"
+    },
+    {
+        url:"https://res.cloudinary.com/dbdrtuscd/image/upload/v1598909790/Screenshot_2020-08-31_at_3.29.39_PM_cz7lnf.png",
+        designer:"Hova",
+        desc:"Red Palazzo Pant",
+        price:"5000",
+        title:"HOVA"
     }
 ]
 export const dress=[
     {
         url:"https://images.unsplash.com/photo-1553816078-25e0948140f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
         designer:"Unknown",
-        desc:"looks reall beautiful on the mannequin",
+        desc:"looks really beautiful on the mannequin",
         price:"40000",
         title:"Designer"
     },
@@ -80,7 +122,49 @@ export const dress=[
         desc:"the mannequin looks elite in this",
         price:"45000",
         title:"Designer"
-    }
+    },
+    {
+        url:"https://images.unsplash.com/photo-1553816078-25e0948140f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"looks really beautiful on the mannequin",
+        price:"40000",
+        title:"Designer"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1588732016705-8a18cb07b557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"the mannequin is pulling it off,so can you",
+        price:"30000",
+        title:"Designer"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1573232695229-f0e8f8c9253f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"the mannequin looks elite in this",
+        price:"45000",
+        title:"Designer"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1553816078-25e0948140f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"looks really beautiful on the mannequin",
+        price:"40000",
+        title:"Designer"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1588732016705-8a18cb07b557?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"the mannequin is pulling it off,so can you",
+        price:"30000",
+        title:"Designer"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1573232695229-f0e8f8c9253f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Unknown",
+        desc:"the mannequin looks elite in this",
+        price:"45000",
+        title:"Designer"
+    },
 ]
 export const suit=[
     {
@@ -103,7 +187,49 @@ export const suit=[
         desc:"Fendi or nothing baby",
         price:"450000",
         title:"Fendi"
-    }
+    },
+    {
+        url:"https://images.unsplash.com/photo-1598808503746-f34c53b9323e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Gucci",
+        desc:"Looks expensive so ....",
+        price:"400000",
+        title:"GUCCI?"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1548917459-67156eade1f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Dior",
+        desc:"Must be expensive sha...",
+        price:"300000",
+        title:"DIOR?"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1589458223095-03eee50f0054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Fendi",
+        desc:"Fendi or nothing baby",
+        price:"450000",
+        title:"Fendi"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1598808503746-f34c53b9323e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Gucci",
+        desc:"Looks expensive so ....",
+        price:"400000",
+        title:"GUCCI?"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1548917459-67156eade1f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Dior",
+        desc:"Must be expensive sha...",
+        price:"300000",
+        title:"DIOR?"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1589458223095-03eee50f0054?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Fendi",
+        desc:"Fendi or nothing baby",
+        price:"450000",
+        title:"Fendi"
+    },
 ]
 export const jeans=[
     {
@@ -126,7 +252,49 @@ export const jeans=[
         desc:"Fendi or nothing baby",
         price:"45000",
         title:"Fendi"
-    }
+    },
+    {
+        url:"https://images.unsplash.com/photo-1554318918-996749a67311?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Denim",
+        desc:"Original denim jeans i guess",
+        price:"40000",
+        title:"DENIM"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1590526396228-3f1e416e665a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Denim",
+        desc:"Dope ass jeans",
+        price:"30000",
+        title:"DENIM"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1591369822096-ffd140ec948f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Fendi",
+        desc:"Fendi or nothing baby",
+        price:"45000",
+        title:"Fendi"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1554318918-996749a67311?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Denim",
+        desc:"Original denim jeans i guess",
+        price:"40000",
+        title:"DENIM"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1590526396228-3f1e416e665a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Denim",
+        desc:"Dope ass jeans",
+        price:"30000",
+        title:"DENIM"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1591369822096-ffd140ec948f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        designer:"Fendi",
+        desc:"Fendi or nothing baby",
+        price:"45000",
+        title:"Fendi"
+    },
 ]
 
 export const sneakers = [
